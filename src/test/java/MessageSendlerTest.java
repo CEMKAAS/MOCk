fc54c19efe233773abadb7adbaf49b5456694ccb
CEMKAAS-Laptop <zaroslikob@mail.ru>
2022-12-01 10:53:37 +0300

    Тест 2 3

--- src/test/java/MessageSendlerTest.java
+++ src/test/java/MessageSendlerTest.java
@@ -1,6 +1,9 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import ru.netology.entity.Country;
 import ru.netology.entity.Location;
 import ru.netology.geo.GeoService;
@@ -12,28 +15,53 @@ import ru.netology.sender.MessageSenderImpl;
 
 import java.util.Map;
 
+
+@ExtendWith(MockitoExtension.class)
 public class MessageSendlerTest {
+    @Mock
+    GeoService geoService = Mockito.mock(GeoServiceImpl.class);
+    LocalizationService localizationService = Mockito.mock(LocalizationServiceImpl.class);
+
     private final String ruIP = "172.123.12.19";
-        private final String usIP = "96.1.1.1";
+    private final String usIP = "96.1.1.1";
 
-        @Test
-        void MessageSenderTest(){
-            GeoService geoService = Mockito.mock(GeoServiceImpl.class);
-            LocalizationService localizationService = Mockito.mock(LocalizationServiceImpl.class);
-            MessageSender messageSender = new MessageSenderImpl(geoService, localizationService);
+    @Test
+    void MessageSenderTest() {
 
-            Mockito.when(localizationService.locale(Country.RUSSIA)).thenReturn("Добро пожаловать");
-            Mockito.when(localizationService.locale(Country.USA)).thenReturn("Welcome");
+        MessageSender messageSender = new MessageSenderImpl(geoService, localizationService);
 
-            Mockito.when(geoService.byIp(ruIP))
-                    .thenReturn(new Location("Moscow", Country.RUSSIA, "Lenina", 15));
-            Mockito.when(geoService.byIp(usIP))
-                    .thenReturn(new Location("New York", Country.USA, " 10th Avenue", 32));
+        Mockito.when(localizationService.locale(Country.RUSSIA)).thenReturn("Добро пожаловать");
+        Mockito.when(localizationService.locale(Country.USA)).thenReturn("Welcome");
 
-            Assertions.assertEquals("Добро пожаловать",
-                    messageSender.send(Map.of(MessageSenderImpl.IP_ADDRESS_HEADER, ruIP)));
-            Assertions.assertEquals("Welcome",
-                    messageSender.send(Map.of(MessageSenderImpl.IP_ADDRESS_HEADER, usIP)));
+        Mockito.when(geoService.byIp(ruIP))
+                .thenReturn(new Location("Moscow", Country.RUSSIA, "Lenina", 15));
+        Mockito.when(geoService.byIp(usIP))
+                .thenReturn(new Location("New York", Country.USA, " 10th Avenue", 32));
 
-        }
+        Assertions.assertEquals("Добро пожаловать",
+                messageSender.send(Map.of(MessageSenderImpl.IP_ADDRESS_HEADER, ruIP)));
+        Assertions.assertEquals("Welcome",
+                messageSender.send(Map.of(MessageSenderImpl.IP_ADDRESS_HEADER, usIP)));
+    }
+
+    @Test
+    void GeoServiceImplTest() {
+        GeoService geoService1 = new GeoServiceImpl();
+
+          Assertions.assertEquals(Country.USA,
+                geoService1.byIp(usIP).getCountry());
+        Assertions.assertEquals(Country.RUSSIA,
+                geoService1.byIp("172.").getCountry());
+
+    }
+
+    @Test
+    void LocalizatorSeviceTest() {
+        LocalizationServiceImpl localizationService1 = new LocalizationServiceImpl();
+
+        Assertions.assertEquals("Welcome", localizationService1.locale(Country.USA));
+        Assertions.assertEquals("Welcome", localizationService1.locale(Country.BRAZIL));
+
+    }
+
 }
